f0b353b6de28cfda2dbff48a86fcd21036326584
Cesar Borges <cesar_silk321@hotmail.com>
2024-09-28 15:02:19 -0300

    first

--- src/permissions/decorator/granted-to.ts
+++ src/permissions/decorator/granted-to.ts
@@ -2,7 +2,10 @@ import 'reflect-metadata';
 import NotAllowed from "../../exceptions/not-allowed";
 
 /**
- * Column decorator used to check a specific class or method can be accessed by the logged in user.
+ * Usado para checar a permissão de acesso a um metodo especifico ou classe.
+ *
+ * Requisito: Classe ou metodo precisa estar encapsulado pela autenticação.
+ *
  */
 export function GrantedTo(role: string) {
   return function (target: any, propertyKey?: string, descriptor?: PropertyDescriptor) {
@@ -12,27 +15,20 @@ export function GrantedTo(role: string) {
       descriptor.value = function (...args: any[]) {
         const {currentUser} = args[0];
         if (!currentUser || currentUser.tipo !== role) {
-          throw new NotAllowed(`Access denied to method. Required role: ${role}`);
+          throw new NotAllowed(`Acesso negado, usuário não possui permissão ao ce acesso ao recurso.`);
         }
         return originalMethod.apply(this, args);
       };
-
       return descriptor;
     } else {
       // Se for uma classe, modifica o comportamento da classe
       Reflect.defineMetadata('role', role, target);
-
       const original = target.prototype;
-
       const newConstructor: any = function (...args: any[]) {
-        console.log(args);
-
-
         const instance = new target(...args);
         instance.requiredRole = role;
         return instance;
       };
-
       newConstructor.prototype = original;
       return newConstructor;
     }
