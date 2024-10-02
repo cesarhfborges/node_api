a1e26e1aa2d66535505b41afd290d093b2787b77
Cesar Borges <cesar_silk321@hotmail.com>
2024-10-02 10:58:14 -0300

    permissions implementado...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  apps : [{
-    name   : "confeccao-api",
-    script : "./dist/index.js"
-  }]
+    apps: [{
+        name: "confeccao-api",
+        script: "./dist/index.js",
+        watch: false,
+        instances: 4,
+        exec_mode: "cluster",
+        env_production: {
+            NODE_ENV: "production"
+        },
+    }]
 }
