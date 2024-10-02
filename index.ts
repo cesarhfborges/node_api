295383ad3d7b288710a53a88d251e49e1bb32b14
Cesar Borges <cesar_silk321@hotmail.com>
2024-10-02 11:28:39 -0300

    Build automatizado

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import "reflect-metadata";
 import * as dotenv from 'dotenv';
 import App from "./src/app";
 
+console.log('GDFHKJSGDFKHJSDGKFJSDGHJFGHSJDKGFKHJSDGFKHJSDF')
+console.log(process.env)
+
 dotenv.config();
 
 const server = new App();
