9dfa39aed655f7c98fcdf142deec47995ed37406
Cesar Borges <cesar_silk321@hotmail.com>
2024-10-02 20:54:46 -0300

    Build automatizado

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "confeccao-api",
-        cwd: './dist/',
+        cwd: './www/',
         script: "./index.js",
         watch: false,
         instances: 4,
